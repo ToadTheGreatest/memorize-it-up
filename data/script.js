afc2edc7cac0b80fbd8991953356e1add0742b53
ToadTheGreatest <jacob.ryanc25@gmail.com>
2026-09-21 19:15:39 -0400

    added show link button

--- data/script.js
+++ data/script.js
@@ -3,31 +3,35 @@ const youtube = "https://youtube.com?q=";
 const full = youtube + videoid;
 const input = document.getElementById("textinput");
 const accuracyout = document.getElementById("accuracy")
+var showable = true;
 var progress = youtube.length;
-var accurate = 0
-var total = 0
-var accuracy = 100
+var accurate = 0;
+var total = 0;
+var accuracy = 100;
 function updateInput(_) {
-    total = 1
+    total = 1;
     if (input.value.length < youtube.length) {
         input.value = youtube;
-        total -= 1
+        total -= 1;
     }
     if (input.value != full.slice(0, progress)) {
         progress += 1;
-        accurate += 1
+        accurate += 1;
+    }
+    if (input.value == full) {
+        window.location.reload();
     }
     if (input.value != full.slice(0, progress - 1)) {
         input.value = full.slice(0, progress - 1);
     }
 }
 function updateAccuracy() {
-    accuracy = (accurate / total) * 100
-    accuracyout.value = toString(accuracy) + "%"
+    accuracy = (accurate / total) * 100;
+    accuracyout.value = toString(accuracy) + "%";
 }
 // NO PASTING WHATSOEVER
 window.addEventListener("paste", (e) => {
     e.preventDefault();
 })
-input.value = youtube
-input.addEventListener("input", updateInput)
+input.value = youtube;
+input.addEventListener("input", updateInput);
