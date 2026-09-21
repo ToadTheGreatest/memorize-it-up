3502b856e684a9c6c35f03c972cc17333dcd9353
ToadTheGreatest <jacob.ryanc25@gmail.com>
2026-09-21 18:58:11 -0400

    fix script

--- data/script.js
+++ data/script.js
@@ -1,17 +1,17 @@
-const videoid = "dQw4w9WgXcQ"
-const youtube = "https://youtube.com?q="
-const full = youtube + videoid
-const input = document.getElementById("textinput")
-var progress = youtube.length
+const videoid = "dQw4w9WgXcQ";
+const youtube = "https://youtube.com?q=";
+const full = youtube + videoid;
+const input = document.getElementById("textinput");
+var progress = youtube.length;
 function updateInput() {
     if (input.value.length < youtube.length) {
-        input.value = youtube
+        input.value = youtube;
     }
-    if (input.value != full.slice(0, progress) {
-        progress += 1
+    if (input.value != full.slice(0, progress)) {
+        progress += 1;
     }
     if (input.value != full.slice(0, progress - 1)) {
-        input.value = full.slice(0, progress - 1)
+        input.value = full.slice(0, progress - 1);
     }
 }
 // NO PASTING WHATSOEVER
