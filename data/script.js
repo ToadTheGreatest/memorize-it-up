fd2f853c86c1907a1fc9768609e89fb4c2a3ba37
ToadTheGreatest <jacob.ryanc25@gmail.com>
2026-09-21 19:04:34 -0400

    added scoring

--- data/script.js
+++ data/script.js
@@ -2,20 +2,32 @@ const videoid = "dQw4w9WgXcQ";
 const youtube = "https://youtube.com?q=";
 const full = youtube + videoid;
 const input = document.getElementById("textinput");
+const accuracyout = document.getElementById("accuracy")
 var progress = youtube.length;
+var accurate = 0
+var total = 0
+var accuracy = 100
 function updateInput(_) {
+    total = 1
     if (input.value.length < youtube.length) {
         input.value = youtube;
+        total -= 1
     }
     if (input.value != full.slice(0, progress)) {
         progress += 1;
+        accurate += 1
     }
     if (input.value != full.slice(0, progress - 1)) {
         input.value = full.slice(0, progress - 1);
     }
 }
+function updateAccuracy() {
+    accuracy = (accurate / total) * 100
+    accuracyout.value = toString(accuracy) + "%"
+}
 // NO PASTING WHATSOEVER
 window.addEventListener("paste", (e) => {
     e.preventDefault();
 })
+input.value = youtube
 input.addEventListener("input", updateInput)
