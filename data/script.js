8aa4535bbabe1617238dd527588f1348512d237d
ToadTheGreatest <jacob.ryanc25@gmail.com>
2026-09-21 20:25:50 -0400

    change the show func to not be one line anymore

--- data/script.js
+++ data/script.js
@@ -43,7 +43,14 @@ function updateAccuracy() {
     accuracy = ((accurate - shows) / total) * 100;
     accuracyout.textContent = accuracy.toFixed(2)+"%";
 }
-function show() {shows += 1;document.getElementById('showbutton').textContent=videoid;setTimeout(() => {document.getElementById('showbutton').textContent='Show Video ID'}, 5000);updateAccuracy();}
+function show() {
+    shows += 1;
+    document.getElementById('showbutton').textContent=videoid;
+    setTimeout(() => {
+        document.getElementById('showbutton').textContent='Show Video ID'
+    }, 5000);
+    updateAccuracy();
+}
 function reset() {
     shows = 0
     textinput.value = youtube;
