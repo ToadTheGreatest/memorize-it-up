86669a668d5bbea324139a589fa7a60545c598ac
ToadTheGreatest <jacob.ryanc25@gmail.com>
2026-09-21 20:18:55 -0400

    added hint system (tips)

--- data/script.js
+++ data/script.js
@@ -2,27 +2,32 @@ const videoid = "dQw4w9WgXcQ";
 const youtube = "https://youtube.com?q=";
 const full = youtube + videoid;
 const textinput = document.getElementById("textinput");
-const accuracyout = document.getElementById("accuracy")
+const accuracyout = document.getElementById("accuracy");
+const hint = document.getElementById("hint");
 var progress = youtube.length;
 var accurate = 0;
 var total = 0;
 var accuracy = 100.0;
 var shows = 0
+function setHint(str) {
+    hint.textContent = str;
+}
 function updateInput(e) {
     total += 1;
     if (textinput.value.length < youtube.length) {
         textinput.value = youtube;
-        progress = youtube.length
+        progress = youtube.length;
         total = 0;
-        console.log("User attempting to delete YouTube link! undoing...")
+        console.log("User attempting to delete YouTube link! undoing...");
     }
     if (textinput.value != full.slice(0, progress + 1)) {
-        console.log(`User got it wrong! Undoing; Metadata:\n-Progress ${progress}\n-Value ${textinput.value}\n-E Value ${e.value}`)
+        console.log(`User got it wrong! Undoing; Metadata:\n-Progress ${progress}\n-Value ${textinput.value}\n-E Value ${e.value}`);
         textinput.value = full.slice(0, progress);
-        console.log(`Conclusion Metadata:\n-Progress ${progress}\n-Value ${textinput.value}\n-E Value ${e.value}`)
+        console.log(`Conclusion Metadata:\n-Progress ${progress}\n-Value ${textinput.value}\n-E Value ${e.value}`);
+        setHint("Nope! Do you need to see it again?")
     }
     if (textinput.value === full) {
-        alert("You did it!")
+        setHint("You did it!")
     }
     if (textinput.value === full.slice(0, progress + 1)) {
         accurate += 1
@@ -30,6 +35,7 @@ function updateInput(e) {
         textinput.value = full.slice(0, progress + 1);
         console.log(`Conclusion Metadata:\n-Progress ${progress}\n-Value ${textinput.value}\n-E Value ${e.value}`)
         progress += 1;
+        setHint("You got it! Keep it up!")
     }
     updateAccuracy()
 }
@@ -44,7 +50,7 @@ function reset() {
     progress = youtube.length;
     accurate = 0;
     total = 0;
-    accuracy = 100;
+    accuracy = 100.0;
 }
 function bruh() {
     window.location.href = youtube.videoid;
